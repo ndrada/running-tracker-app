30e28a333eebe124b78393f5eed1b006b8e8536b
ndrada <andrada.groseanu@gmail.com>
2023-11-20 07:10:50 -0800

    updated the program
    adjusted some of the JS functionality, added a progress bar and some logic for displaying a message based on your performance. App is not finished yet but looking good!

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const goal = 25;
+const tolerance = 0.5;
 let entries = []
 const entriesWrapper = document.querySelector('#entries');
+document.querySelector('#target').innerText = goal;
 
 function addNewEntry(newEntry){
     entriesWrapper.removeChild(entriesWrapper.firstElementChild);
@@ -14,15 +17,50 @@ function reducer(total, currentValue){
     return total + currentValue;
 }
 
+function updateProgressBar(totalMiles, goal) {
+
+    const progressBar = document.getElementById('progressBar');
+    const percentage = Math.min((totalMiles / goal) * 100, 100);
+
+    progressBar.style.width = percentage.toFixed(1) + '%';
+    
+    const progressBarContainer = document.querySelector('.progressBarContainer');
+    const setTargetElements = document.querySelector('.setTarget');
+    const targetMetElements = document.querySelector('.targetMet');
+    const targetNotMet = document.querySelector('.targetNotMet');
+    const exceededTarget = document.querySelector('.targetExceeded');
+
+
+    if (totalMiles >= goal - tolerance && totalMiles <= goal + tolerance && entries.length >= 7) {
+        setTargetElements.style.display = 'none';
+        progressBarContainer.style.display = 'none';
+        targetMetElements.style.display = 'flex';
+    } else if (totalMiles < goal && entries.length >= 7){
+        setTargetElements.style.display = 'none';
+        progressBarContainer.style.display = 'none';
+        targetNotMet.style.display = 'flex';
+    } else if (totalMiles > goal && entries.length >= 7){
+        setTargetElements.style.display = 'none';
+        progressBarContainer.style.display = 'none';
+        exceededTarget.style.display = 'flex';
+    }
+}
+
 function calcTotal(){
-    const totalValue = entries.reduce(reducer);
-    document.getElementById('total').innerText = totalValue;
-    document.getElementById('progressTotal').innerText = totalValue;
+    const totalValue = entries.reduce(reducer, 0);
+    document.getElementById('total').innerText = totalValue.toFixed(1);
+    document.getElementById('progressTotal').innerText = totalValue.toFixed(1);
+    updateProgressBar(totalValue, goal);
 }
 
 function calcAverage(){
     const average = entries.reduce(reducer, 0) / entries.length;
     document.getElementById('average').innerText = average.toFixed(1);
+}
+
+function weeklyHigh(){
+    const high = Math.max(...entries);
+    document.getElementById('high').innerText = high;
 }
 
 function handleSubmit(event) {
@@ -34,6 +72,11 @@ function handleSubmit(event) {
     addNewEntry(entry);
     calcTotal();
     calcAverage();
+    weeklyHigh();
+}
+
+function newWeek(){
+    location.reload();  
 }
 
 const form = document.querySelector("form").addEventListener('submit', handleSubmit);
